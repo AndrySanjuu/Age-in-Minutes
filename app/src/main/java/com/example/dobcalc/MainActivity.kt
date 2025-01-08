4a97ad5811bbb57f880ac394b15c0bfc95de4f73
Andrei Mihai <a.mihai@reply.it>
2025-01-08 15:06:03 +0100

    init commit

---
+++ app/src/main/java/com/example/dobcalc/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.dobcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{
+            clickDatePicker()
+
+
+        }
+
+
+    }
+
+
+    fun clickDatePicker(){
+
+
+
+
+        Toast.makeText(this,
+            "btnDatePicker Pressed", Toast.LENGTH_LONG).show()
+    }
+
+}
